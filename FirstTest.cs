0da42be4e6ad0ab88690c96ea61e102c79e561ca
Alena Belyavskaya <alena.velitskaya@gmail.com>
2019-04-06 20:25:05 +0400

    Google search/VK login

---
+++ FirstTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace seleniumtest
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "http://www.google.com";
+            driver.FindElement(By.Name("q")).SendKeys("webdriver");
+            driver.FindElement(By.Name("btnK")).Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ VkLogin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace seleniumtest
+{
+    public class VkLogin
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://vk.com/";
+
+            IWebElement username = driver.FindElement(By.Id("index_email"));
+            username.SendKeys("alena.velitskaya@gmail.com");
+
+            IWebElement password = driver.FindElement(By.Id("index_pass"));
+            password.SendKeys("123");
+
+            IWebElement login = driver.FindElement(By.Id("index_login_button"));
+            login.Click();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
